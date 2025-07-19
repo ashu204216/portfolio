231fdfccd1500061f362d6797b45e31ef85f33b0
ashu204216 <ashutoshkaushik347@gmail.com>
2025-07-19 14:53:58 +0530

    build fix

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-// Serve static files from the build directory
-app.use(express.static(path.join(__dirname, 'build')));
+// Serve static files from the dist directory
+app.use(express.static(path.join(__dirname, 'dist')));
 
 // For React Router (SPA) support
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 app.listen(PORT, () => {
